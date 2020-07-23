b50aa274e504790f33416014287c1c25ae1d3507
Glitch (amused-shaded-raclette) <none>
2020-07-23 12:01:12 +0000

    🌿😄 Checkpoint ./public/translator.js:9596039/289

--- public/translator.js
+++ public/translator.js
@@ -14,20 +14,25 @@ let BtoA2 = Object.keys(britishToAmericanTitles);
 let option = document.getElementById('locale-select');
 
 document.getElementById('translate-btn').addEventListener('click', (e) => {
-  let sentenceArray = textArea.value.split(" ");
-  if(option.value === 'american-to-british') {
-    for(let i = 0; i < sentenceArray.length; i++) {
-      AtoB1.map((d, j) => {
-        if(d === sentenceArray[i]) {
-          sentenceArray.splice(i, 1, americanOnly[d])
-          console.log(americanOnly[d]);
-          console.log(sentenceArray);
-        }
-      })
-    }
-  } else if(option.value === 'british-to-american') {
+//   let sentenceArray = textArea.value.split(" ");
+//   if(option.value === 'american-to-british') {
+//     for(let i = 0; i < sentenceArray.length; i++) {
+//       AtoB1.map((d, j) => {
+//         if(d === sentenceArray[i]) {
+//           sentenceArray.splice(i, 1, americanOnly[d])
+//           console.log(americanOnly[d]);
+//           console.log(sentenceArray);
+//         }
+//       })
+//     }
+//   } else if(option.value === 'british-to-american') {
     
-  }
+//   }
+  let str = textArea.value;
+   AtoB1.map((d, j) => {
+     let res = str.replace(new RegExp(d, "g"), americanOnly[d])
+     console.log(res);
+   })
 })
 
 /* 
