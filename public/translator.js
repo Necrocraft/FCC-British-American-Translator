53017bbffa8c0036d2463c8e0aa6b6fca270722e
Glitch (amused-shaded-raclette) <none>
2020-07-23 07:40:58 +0000

    📠🌜 Checkpoint ./public/translator.js:9596039/402

--- public/translator.js
+++ public/translator.js
@@ -3,13 +3,17 @@ import { britishOnly } from './british-only.js';
 import { americanToBritishSpelling } from './american-to-british-spelling.js';
 import { americanToBritishTitles } from './american-to-british-titles.js';
 
+let textArea = document.getElementById('text-input');
+
+textArea.addEventListener('input', e => {
+  let sentenceArray = textArea.value.split(" ");
+  for(let i = 0; i < sentenceArray.length; i++) {
+    
+  }
+})
+
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
-try {
-  module.exports = {
-
-  }
-} catch (e) {}
